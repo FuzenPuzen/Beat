46440782f34b9dc2354a8f7edd8d609bb772ab02
minilabapps01 <minilabapps@gmail.com>
2021-02-01 00:18:06 +0300

    12

---
+++ Assets/Scripts/Equlizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equlizer : MonoBehaviour
+{
+    [SerializeField] List<GameObject> beats;
+    int i;
+
+    void Start()
+    {
+        i = -1;
+        StartCoroutine(Timer());
+    }
+
+
+    public void replay()
+    {
+        i = -1;
+        StartCoroutine(Timer());
+    }
+    public void Go()
+    {
+        beats[i].GetComponent<Scalling>().StartScalling();
+        StartCoroutine(Timer());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(0.2f);
+        i++;
+        Go();
+    }
+}
---
+++ Assets/Scripts/Scalling.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scalling : MonoBehaviour
+{
+    [SerializeField] private float yScale;
+    [SerializeField] private float scaleSpeed;
+    float buff;
+    void Start()
+    {
+        buff = yScale;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartScalling()
+    {
+        StartCoroutine(Scaling());
+    }
+
+    public IEnumerator Scaling()
+    {
+        while (transform.localScale.y <= yScale)
+        {
+            transform.localScale += new Vector3(0, scaleSpeed * Time.deltaTime, 0);
+            yield return null;
+        }
+        SwitchDirection(1);
+    }
+
+    public IEnumerator DeScaling()
+    {
+        while (transform.localScale.y >= 1)
+        {
+            transform.localScale -= new Vector3(0, scaleSpeed * Time.deltaTime, 0);
+            yield return null;
+        }
+        //SwitchDirection(2);
+    }
+
+    public void SwitchDirection(int _i)
+    {
+        if (_i == 1)
+        {
+            StartCoroutine(DeScaling());
+        }
+        else
+        {
+            StartCoroutine(Scaling());
+        }
+    }
+}
